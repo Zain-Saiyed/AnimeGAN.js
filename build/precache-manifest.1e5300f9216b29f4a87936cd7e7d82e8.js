16f4ecf5e2e52fe831ceeb5a705c224ab8f7d83f
EC2 Default User <ec2-user@ip-10-0-3-126.ec2.internal>
2023-12-01 22:21:58 +0000

    Adding build folder

---
+++ build/precache-manifest.1e5300f9216b29f4a87936cd7e7d82e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094f1032b2139ae4c4a0cc7d855b245c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fc32d625abf1fa8e01f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "05f1dc98609add100500",
+    "url": "/static/css/main.a2ee3e59.chunk.css"
+  },
+  {
+    "revision": "7fc32d625abf1fa8e01f",
+    "url": "/static/js/2.02bfdca5.chunk.js"
+  },
+  {
+    "revision": "5e2252a167034352fa7bed0a873eaa75",
+    "url": "/static/js/2.02bfdca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f1dc98609add100500",
+    "url": "/static/js/main.a7e1317e.chunk.js"
+  },
+  {
+    "revision": "ae2e66fb743c7f9d8c82",
+    "url": "/static/js/runtime-main.f1dfb799.js"
+  }
+]);
